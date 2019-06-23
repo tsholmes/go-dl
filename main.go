38a943cfdde8f0f052f857b354d471270f2d20be
Tom Holmes <tom@wandb.com>
2019-06-22 22:25:11 -0700

    bigger example

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	x := tensor.Input(10)
+	x := tensor.Input(1000)
 
 	targetMean := tensor.Input(1)
 	targetStddev := tensor.Input(1)
@@ -29,10 +29,12 @@ func main() {
 		0.5,
 	)
 
-	meanErr := tensor.PowConstant(tensor.Sub(mean, targetMean), 2.)
-	stdErr := tensor.PowConstant(tensor.Sub(stddev, targetStddev), 2.)
+	// meanErr := tensor.PowConstant(tensor.Sub(mean, targetMean), 2.)
+	// stdErr := tensor.PowConstant(tensor.Sub(stddev, targetStddev), 2.)
+	meanErr := tensor.Abs(tensor.Sub(mean, targetMean))
+	stdErr := tensor.Abs(tensor.Sub(stddev, targetStddev))
 
-	curX := calc.RandomUniform(-1.0, 1.0, 10)
+	curX := calc.RandomUniform(-1.0, 1.0, 1000)
 	curMeanTgt := calc.Zeros(1)
 	curStddevTgt := calc.Ones(1)
 
@@ -49,7 +51,7 @@ func main() {
 		m, std, grad := val[0], val[1], val[2]
 
 		if (i+1)%100 == 0 {
-			fmt.Println(curX, m, std)
+			fmt.Println(m, std)
 		}
 
 		curX = curX.Add(grad.MulConstant(-0.1))
