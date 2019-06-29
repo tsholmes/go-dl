d327bb620c4a77c02403d1dca59c9de793ef5d7f
Tom Holmes <tom@wandb.com>
2019-06-29 15:03:26 -0700

    speed up inverse conv blas

--- calc/blas.go
+++ calc/blas.go
@@ -67,41 +67,43 @@ func blasInverseConv2D(a NDArray, g NDArray, arr NDArray) {
 
 	iWOff := a.shape[adim-1]
 	iHOff := iWOff * a.shape[adim-2]
+	iBOff := iHOff * a.shape[adim-3]
+
+	gWOff := g.shape[adim-1]
+	gHOff := gWOff * g.shape[adim-2]
+	gBOff := gHOff * g.shape[adim-3]
 
 	kFOff := 1
 	kIFOff := kFOff * kf
 	kWOff := kIFOff * inf
 	kHOff := kWOff * kw
 
-	g.ForEach(func(dataIndex int, index []int, value float64) {
-		if index[adim-1] != 0 {
-			return
-		}
-		iDataIndex := a.dataIndex(index)
-
-		y := blas64.Vector{
-			N:    kf,
-			Data: g.data[dataIndex : dataIndex+kf],
-			Inc:  1,
-		}
-
-		for h := 0; h < kh; h++ {
-			for w := 0; w < kw; w++ {
-				pIndex := iDataIndex + h*iHOff + w*iWOff
-				kIndex := h*kHOff + w*kWOff
-				x := blas64.Vector{
-					N:    inf,
-					Data: a.data[pIndex : pIndex+inf],
+	for iBIndex, gBIndex := 0, 0; iBIndex < len(a.data); iBIndex, gBIndex = iBIndex+iBOff, gBIndex+gBOff {
+		for r := 0; r < g.shape[adim-3]; r++ {
+			for c := 0; c < g.shape[adim-2]; c++ {
+				gDataIndex := gBIndex + r*gHOff + c*gWOff
+				y := blas64.Vector{
+					N:    kf,
+					Data: g.data[gDataIndex : gDataIndex+kf],
 					Inc:  1,
 				}
-				a := blas64.General{
-					Rows:   inf,
-					Cols:   kf,
-					Data:   arr.data[kIndex : kIndex+inf*kf],
-					Stride: kf,
+				for h := 0; h < kh; h++ {
+					iDataIndex := iBIndex + (r+h)*iHOff + c*iWOff
+					kIndex := h * kHOff
+					x := blas64.Vector{
+						N:    inf,
+						Data: a.data[iDataIndex : iDataIndex+inf*kw],
+						Inc:  1,
+					}
+					a := blas64.General{
+						Rows:   inf * kw,
+						Cols:   kf,
+						Data:   arr.data[kIndex : kIndex+inf*kf*kw],
+						Stride: kf,
+					}
+					blas64.Ger(1.0, x, y, a)
 				}
-				blas64.Ger(1.0, x, y, a)
 			}
 		}
-	})
+	}
 }
